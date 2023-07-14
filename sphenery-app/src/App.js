f14eb4df06344fd93c64074d1b145c710ad8b705
kalecream <sabrinamedwinter@gmail.com>
2023-07-14 17:24:10 -0500

    fix: Rework Routes

--- sphenery-app/src/App.js
+++ sphenery-app/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Register from './components/Register';
 import Login from './components/Login';
 import UserList from './components/Users';
 
@@ -13,13 +12,14 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/register" element={token.accessToken ? <Navigate to="/users" /> : <Register />} />
                 <Route path="/login" element={token.accessToken ? <Navigate to="/users" /> : <Login setToken={setToken} />} />
                 <Route path="/users" element={token.accessToken ? <UserList token={token} setToken={setToken} /> : <Navigate to="/login" />} />
                 <Route path="/" element={token.accessToken ? <Navigate to="/users" /> : <Navigate to="/login" />} />
-            </Routes>
-        </Router>
+            </Routes> 
+         </Router>
     );
 }
 
 export default App;
+
+
